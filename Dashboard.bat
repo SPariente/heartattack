4767890e696fce70f93150aacaab15baa23d7e3f
SPariente <samuel.pariente@hotmail.fr>
2021-12-18 14:14:14 +0100

    streamlined packages installation line

--- Dashboard.bat
+++ Dashboard.bat
@@ -2,12 +2,7 @@
 title Heart disease dashboard launcher
 echo This will install the required python packages, download the dashboard script, and run it
 python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install pandas
-python3 -m pip install numpy
-python3 -m pip install matplotlib
-python3 -m pip install plotly_express
-python3 -m pip install dash
-python3 -m pip install statsmodels
+python3 -m pip install pandas numpy matplotlib plotly_express dash statsmodels
 powershell -c "invoke-webrequest 'https://raw.githubusercontent.com/SPariente/heartattack/master/dashboard.py' -outfile ./dashboard.py"
 start http://127.0.0.1:8050/
 echo --------------------------------------------------
